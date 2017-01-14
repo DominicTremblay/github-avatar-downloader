738e77e4ad4def6e892fa03438b8639bfe725237
DominicTremblay <dominictremblay24@gmail.com>
2017-01-14 09:57:49 -0500

    getting the json file from the api

---
+++ download_avatars.js
@@ -0,0 +1,32 @@
+var GITHUB_USER = "DominicTremblay";
+var GITHUB_TOKEN = "2c53aa22f54d3b74ece0dfab62afc3952dae46eb";
+
+var request = require('request');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var options = {
+        url: requestURL,
+        headers: {
+          'User-Agent': 'request'
+        }
+      }
+
+  request(options, function(err, response, body){
+
+    if(err) {
+      cb(err,null);
+    }
+    else {
+      console.log(response.statusCode);
+      cb(null, JSON.parse(body));
+    }
+  });
+}
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
