2b6d0f44a31f4fd12c26852813e123ef8561c285
DominicTremblay <dominictremblay24@gmail.com>
2017-01-14 13:41:24 -0500

    downloadByURL function is done

--- download_avatars.js
+++ download_avatars.js
@@ -2,6 +2,7 @@ var GITHUB_USER = "DominicTremblay";
 var GITHUB_TOKEN = "2c53aa22f54d3b74ece0dfab62afc3952dae46eb";
 
 var request = require('request');
+var fs = require('fs');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
@@ -24,18 +25,36 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
+function downloadImageByURL(url, filePath) {
+  request(url)
+  .on('error', function(err){
+    throw err;
+  })
+  .on('response', function(response){
+    console.log('Downloading image...');
+    console.log('HTTP Status', response.statusCode, response.statusMessage);
+    console.log('HTTP Content-Type: \'' + response.headers['content-type'] + '\'');
+  })
+  .pipe(fs.createWriteStream(filePath))
+  .on('finish', function(){
+    console.log("Download complete");
+  });
+}
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  if (err) {
-    console.log("Errors:", err);
-  }
+downloadImageByURL('https://avatars2.githubusercontent.com/u/2741?v=3&s=466', 'avatars/kvirani.jpg');
 
-  else {
 
-    for (contributorKey in result) {
-      console.log(result[contributorKey].avatar_url);
-    }
-  }
+// getRepoContributors("jquery", "jquery", function(err, result) {
+//   if (err) {
+//     console.log("Errors:", err);
+//   }
 
-});
+//   else {
+
+//     for (contributorKey in result) {
+//       console.log(result[contributorKey].avatar_url);
+//     }
+//   }
+
+// });
 
